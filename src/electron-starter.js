8baa5841ef83c7e6bea4bb1a5983e79e052ecd29
zc1415926 <zc1415926@126.com>
2019-02-11 12:46:28 +0800

    set title, set nodeIntegration to true

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -47,7 +47,15 @@ ipcMain.on('write-open-py-file', (event, arg)=>{
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({
+        width: 800, 
+        height: 600, 
+        title: 'Python Classwork Explorer',
+        webPreferences:
+        {
+            nodeIntegration: true
+        }}
+    );
 
     const startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '/../build/index.html'),
